e49f8ae3e757f99c4fdaaa6f6c0e66733efd9d09
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2021-10-15 16:11:55 +0530

    Added some more content

--- DSA/C++ STL/STL.cpp
+++ DSA/C++ STL/STL.cpp
@@ -118,42 +118,232 @@ int main(){
 
     //3D vector of dimension 10x20x30
     vector<vector<vector<int>>> vec1(10,vector<vector<int>> (20,vector<int> (30,0)));
+
+    //set
+    // ONLY STORES UNIQUE ELEMENTS IN ASCENDING ORDER
+    //it helps to store the unique elements
+    //and we cannot access set element by set[0] but we can access set elements by iterartors like st.begin() etc
+    set<int> st;
+    int n,x;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        st.insert(x);//it takes complexity of log(n) here n is the size of the set
+    }
+
+    //erase functionality
+    //complexity of this is simple logn
+    st.erase(st.begin()); //this will erase the first elemet from the set
+    st.erase(st.begin(),st.begin() + 2); //-> [)
+
+    st.erase(5);// st.erase(key) it will delete teh particular key 
+
+    set<int> st1={1,5,7,8};
+    auto it = st1.find(7); //logn it will be iterator to 7
+    auto it1 = st1.find(9); //it=st.end() which is the next right after the last element
+
+    st1.size();//it will print the size of the set
+
+    // two ways to implement for loop for set
+    for(auto it=st.begin();it!=st.end();it++){
+        cout<<*it<<" ";
+    }
+
+    for(auto it : st){
+        cout<<it<<endl;
+
+    }
+
+    //unordered set
+
+    unordered_set<int> st2;
+    // the only thing that is good in unordered set is that the average time complexity is O(1) whereas it is log(n) in ordered set
+    //but the worst case is linear in nature , O(set size)
+
+    //pro tip-> if in the question there is no need to store the element in ascending order then always use unordered set 
+    //if tle occured then switch to ordered set 
+
+    //Multiset
+    //it will store all the elements in the sorted fashion
+
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(3);
+    //-> {1,1,2,2,3}
+    ms.erase(2);// it will delete all the instance of 2 from the set
+
+    auto it=ms.find(2);// it will return the iterator pointing to the first instance of 2
+
+    // it is log n in size
+
+    st.count(2); // it will count how many times the instance occurs
+
+    //Pair
+    pair<int,int> pr;
+    pr.first=1;
+    pr.second = 2;
+
+    // Map
+    // iT stores everything in sorted order
+    // it will sort according to the keys
+    //map only stores unique keys
+    // log n is the time complexity of map
+    map<string,int> mpp;
+    mpp["jay"]=1;
+    mpp.erase("jay"); //mpp.erase(key)
+    mpp.erase(mpp.begin());//mpp.erase(iterator)
+    mpp.clear(); //entire map is clear 
+    auto it = mpp.find("jay");
+
+    mpp.empty(); //to check if map is empty or not
+
+    //printing map
+    for(auto it:mpp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+
+    for(auto it =mpp.begin(); it!=mpp.end();it++){
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+
+    //pair class 
+    pair<int, int> pr ={1,2};
+    pair<pair<int,int>,int> pr1 = {{2,3},4};
+    cout<<pr1.first.second<<endl;
+
+    // stack and queue
+    stack<int> sta;//lifo 
+    //pop
+    //top
+    //size
+    //empty
+    //push/emplace
+
+    sta.push(1);
+    sta.push(2);
+    sta.push(3);
+    sta.push(4);
+    sta.push(5);
+
+    cout<<sta.top();
+    sta.pop();
+
+
+    //Delete entire stack
+    while(!sta.empty()){
+        sta.pop();
+    }
+
+    cout<<sta.size()<<endl;//give the size of the stack
+
+
+    stack<int> sta1;
+    // always check the below condition while implementing stack.top() operation
+    if(!sta.empty()){
+         sta1.top(); //it will throw an error
+    }
+
+    //queue fifo
+    // all the given operation take O(1) time complexity
+    //push
+    //front
+    //pop
+    //size
+    //empty
+
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    q.front();//->it print 1
+
+    //take O(n) where n is the size of the element
+    while(!q.empty()){
+        q.pop();
+    }
+
+    //Priority Queue
+    // stores all in sorted order and does all the operation in log(n) time complexity
+    //push
+    //size
+    //top
+    //pop
+    //empty
+    priority_queue<int> pq;
+    pq.push(1);
+    pq.push(5);
+    pq.push(2);
+    pq.push(6);
+    //contains all the element in descending order -> {6,5,2,1}
+
+    pq.top(); //it will print 6
+
+    //minimum priority queue
+    priority_queue<int,vector<int> , greater<int>>> pq1;
+
+
+    //deque
+    // doubly ended queue
+    //push_front();
+    //push_back();
+    //pop_front();
+    //pop_back();
+    //begin,rbegin,end,rend
+    //size
+    //clear
+    //empty
+    //at
+    deque<int> dq;
     
 
+    //list
+     //push_front();
+    //push_back();
+    //pop_front();
+    //pop_back();
+    //begin,rbegin,end,rend
+    //size
+    //clear
+    //empty
+    //at
+    //remove() -> O(1)
+    list<int> ls;
+
+
+    //Q Given n number of elememnts that occurs maximum number of times 
+    //input
+    //5
+    //1 3 3 3 2
+
+    //output
+    //3
+
+    int n;
+    cin>>n;
+    map<int,int> mpp12;
+    int maxi=0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        mpp12[x]++;
+        if(mpp12[x]>mpp12[maxi]){
+            maxi=x;
+        }
+            
+
+    }
+    cout<<x<<endl;
+
+    //here the time complexity in n*log(n)
 
 
 
 
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-  
    
-
-
-
-
-
-
-
-
-
-
 
 
 
