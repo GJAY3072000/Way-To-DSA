a3904e7fcc4473139fc482308990c13f9dd258ba
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2021-10-22 20:39:17 +0530

    update

---
+++ DSA/Leetcode/Array/Q4.cpp
@@ -0,0 +1,16 @@
+//Merge Sorted Array
+//https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
+// try it once more
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         int i = m - 1, j = n - 1, tar = m + n - 1;
+        while (j >= 0) {
+            nums1[tar--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+        
+    }
+};
