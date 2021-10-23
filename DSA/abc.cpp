b4929db3e52c361586e8c6ba9053d0bf59349c93
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2021-10-23 22:43:49 +0530

    update

---
+++ DSA/abc.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(string &S,int n){
+    for(int i=0;i<=n;i++){
+        if(S[i]=='e' && S[i+1]=='r'){
+            cout<<"er"<<endl;
+        }
+        if(S[i]=='i'&&S[i+1]=='s'&& S[i+2]=='t'){
+            cout<<"ist"<<endl;
+            
+    }
+
+
+
+}
+}
+
+int main(){
+
+    string S;
+    int n=S.length();
+    solve(S,n);
+
+
+}
