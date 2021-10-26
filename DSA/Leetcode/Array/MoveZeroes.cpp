6bcb3d76678a0001cb3d4297e575494c87a52bdc
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2021-10-26 16:59:13 +0530

    update

---
+++ DSA/Leetcode/Array/MoveZeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    int last = 0, cur = 0;
+    while(cur < nums.size()) {
+        if(nums[cur] != 0) {
+            swap(nums[last], nums[cur]);
+            last++;
+        }
+        cur++;
+    }
+ }
+};
