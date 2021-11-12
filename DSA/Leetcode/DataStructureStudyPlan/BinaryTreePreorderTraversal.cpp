3d04d1022e177a56f415bb3d5f5d4b6c26cf95a2
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2021-11-12 19:13:55 +0530

    Update BinaryTreePreorderTraversal.cpp

--- DSA/Leetcode/DataStructureStudyPlan/BinaryTreePreorderTraversal.cpp
+++ DSA/Leetcode/DataStructureStudyPlan/BinaryTreePreorderTraversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Recursive Solution
 class Solution {
 public:
     void Solve(TreeNode* root,vector<int> & ans){
@@ -23,5 +24,36 @@ public:
         Solve(root,ans);
         return ans;
         
+    }
+};
+
+//Iterative method
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        s.push(root);
+        while(s.size()){
+            auto a=s.top();
+            s.pop();
+            ans.push_back(a->val);
+            if(a->right)s.push(a->right);
+            if(a->left)s.push(a->left);
+        }
+        return ans; 
     }
 };
