b54cff3fcad5134669c2891c8fc683fc6e6fab70
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-02-28 13:17:53 +0530

    Create 228. Summary Ranges.cpp

---
+++ Leetcode/228. Summary Ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string getStr(int first, int last) {
+        return first == last ? to_string(first) : to_string(first) + "->" + to_string(last);
+    }
+    
+    vector<string> summaryRanges(vector<int>& nums) {
+        if (nums.size() == 0)
+            return vector<string>();  
+        vector<string> res;
+        int first = nums[0];
+        for (int i=1; i<nums.size(); ++i) {
+            if (nums[i-1]+1 != nums[i]) {
+                res.push_back(getStr(first, nums[i-1]));
+                first = nums[i];
+            }
+        }
+        res.push_back(getStr(first, nums.back()));
+        return res;
+    }
+};
