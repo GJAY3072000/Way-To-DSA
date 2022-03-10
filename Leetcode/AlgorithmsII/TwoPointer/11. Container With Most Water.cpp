cad586044cd8101208312d268c6c65cf1ca2a657
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-03-10 11:19:57 +0530

    Create 11. Container With Most Water.cpp

---
+++ Leetcode/AlgorithmsII/TwoPointer/11. Container With Most Water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+         int ans = 0, i = 0, j = height.size()-1, res = 0;
+        while (i < j) {
+            if (height[i] <= height[j]) {
+                res = height[i] * (j - i);
+                i++;
+            } 
+            else {
+                res = height[j] * (j - i);
+                j--;
+            }
+            if (res > ans) 
+                ans = res;
+        }
+        return ans;
+    }
+};
