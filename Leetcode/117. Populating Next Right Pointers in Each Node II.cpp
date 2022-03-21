d5ac3b01a492f13787371ee0dc05d9c3779df563
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-03-21 11:43:09 +0530

    Update 117. Populating Next Right Pointers in Each Node II.cpp

--- Leetcode/117. Populating Next Right Pointers in Each Node II.cpp
+++ Leetcode/117. Populating Next Right Pointers in Each Node II.cpp
@@ -6,11 +6,8 @@ public:
     Node* left;
     Node* right;
     Node* next;
-
     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
     Node(int _val, Node* _left, Node* _right, Node* _next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
@@ -21,13 +18,16 @@ public:
     Node* connect(Node* root) {
         Node *currParent = root, *baseChild, *currChild, *nextChild;
         while (currParent) {
-            while (currParent->next && !currParent->left && !currParent->right) currParent = currParent->next;
+            while (currParent->next && !currParent->left && !currParent->right) 
+                currParent = currParent->next;
             currChild = baseChild = currParent->left ? currParent->left : currParent->right;
             while (currChild) {
-                if (currParent->right && currChild != currParent->right) nextChild = currParent->right;
+                if (currParent->right && currChild != currParent->right) 
+                    nextChild = currParent->right;
                 else {
                     currParent = currParent->next;
-                    while (currParent && !currParent->left && !currParent->right) currParent = currParent->next;
+                    while (currParent && !currParent->left && !currParent->right)
+                        currParent = currParent->next;
                     nextChild = currParent ? currParent->left ? currParent->left : currParent->right : currParent;
                 }
                 currChild->next = nextChild;
