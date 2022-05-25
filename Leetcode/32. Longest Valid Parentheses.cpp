fb6eb50c9f23d1f0f9af4c70beb0303e366c2e42
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-05-25 09:29:42 +0530

    Create 32. Longest Valid Parentheses.cpp

---
+++ Leetcode/32. Longest Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int maxVal = 0;
+    	stack<int> st;
+	    st.push(-1);
+    	for(int i = 0; i < s.size(); i++)
+	    	if(s[i] == '(')
+                st.push(i);
+    		else {       
+	    		st.pop();
+		    	if(st.empty())
+                    st.push(i);
+    			else
+                    maxVal = max(maxVal, i - st.top());
+    		}        
+	    return maxVal;
+    }
+};
