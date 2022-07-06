57c73f0981af68ed06125c550b7325fce6859b49
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-07-06 20:02:41 +0530

    Update 2190. Most Frequent Number Following Key In an Array.cpp

--- Leetcode/2190. Most Frequent Number Following Key In an Array.cpp
+++ Leetcode/2190. Most Frequent Number Following Key In an Array.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
-        int answer=-1;
-        int res=INT_MIN;
-        int n=nums.size();
-        int count=1;
-        
-        if(nums.size()==2){
-            return nums[1];
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==key)
+                m[nums[i+1]]++;
         }
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]==key){
-                int target=nums[i+1];
-                for(int k=i+1;k<n;k++){
-                    if(nums[k]==key && nums[k+1]==target){
-                        count++;
-                    }
-                }
-                if(res<count){
-                    answer=target;
-                    res=max(res,count);
-                }
+        int freqCount=0;
+        int ans;
+        for(auto &temp : m){
+            if(temp.second > freqCount){
+                freqCount = temp.second;
+                ans = temp.first;
             }
         }
-        return answer;
+        return ans;  
     }
 };
