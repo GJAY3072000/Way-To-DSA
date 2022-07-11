a122baf5279eb843f735060c3596e2e755b18248
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-07-11 21:47:33 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Leetcode/199. Binary Tree Right Side View.cpp
+++ Leetcode/199. Binary Tree Right Side View.cpp
@@ -11,17 +11,20 @@
  */
 class Solution {
 public:
+
+    void reversePreorder(TreeNode* root, int level , vector<int> &res){
+        if(root == NULL)
+            return;
+        if(res.size() == level){
+            res.push_back(root->val);
+        }
+        reversePreorder(root->right,level+1,res);
+        reversePreorder(root->left,level+1,res);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
         vector<int> res;
-        recursion(root,0,res);
+        reversePreorder(root,0,res);
         return res;
     }
-    
-    void recursion(TreeNode* root,int level,vector<int> &res){
-        if(root==NULL) return;
-        if(res.size()==level) res.push_back(root->val);
-        recursion(root->right,level+1,res);
-        recursion(root->left,level+1,res);
-    }
 };
-  
