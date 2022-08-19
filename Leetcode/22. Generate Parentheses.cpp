23c7e2e3182cd88637ceac893cd4f6577ad36a57
GJAY3072000 <53868821+GJAY3072000@users.noreply.github.com>
2022-08-19 14:23:25 +0530

    Update 22. Generate Parentheses.cpp

--- Leetcode/22. Generate Parentheses.cpp
+++ Leetcode/22. Generate Parentheses.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    vector<string>result;
-    
-    void helper(int open,int close,int n,string current)
-    {
-        if(current.length()==n*2)
-        {
-            result.push_back(current);
+    void solve(int open ,int close,string op,vector<string>&v){
+        if(open ==0 && close==0){
+            v.push_back(op);
             return;
         }
-        if(open<n)  helper(open+1,close,n,current+"(");
-        if(close<open)  helper(open,close+1,n,current+")");
+        if(open!=0){
+            string op1=op;
+            op1.push_back('(');
+            solve(open-1,close,op1,v);
+        }
+        if(close>open){
+            string op2=op;
+            op2.push_back(')');
+            solve(open,close-1,op2,v);
+        }
     }
     vector<string> generateParenthesis(int n) {
-        helper(0,0,n,"");
-        return result;
+        vector<string> v;
+        int close=n;
+        int open = n;
+        string op="";
+        solve(open,close,op,v);
+        return v;
     }
 };
